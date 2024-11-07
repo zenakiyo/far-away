24045b782db78f1fdb6cd1211af6ef2e5134f642
zenakiyo <zenakiyo@gmail.com>
2024-11-07 13:38:41 +0530

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🛍️</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      🛍️ You have x items on your list, and you already packed x (x%)
+    </footer>
+  );
+}
